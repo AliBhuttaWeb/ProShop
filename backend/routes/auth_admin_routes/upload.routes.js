393b0cb0b6c8685446bbe77d3ee69869c9014a30
Ali Bhutta <ali.bhutta@invozone.com>
2022-01-23 19:20:00 +0500

    separated upload function

--- backend/routes/auth_admin_routes/upload.routes.js
+++ backend/routes/auth_admin_routes/upload.routes.js
@@ -3,22 +3,9 @@ const router  = express.Router();
 const authMiddleware = require('../../middleware/auth.middleware');
 const adminMiddleware = require('../../middleware/admin.middleware');
 const UploadController = require('../../controllers/upload.controller');
-const multer  = require('multer')
-const path    = require('path');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/uploads/images/')
-    },
-
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    }
-});
-const upload = multer({ storage: storage });
+const Helper = require('../../utils/helper');
 
 router.use(authMiddleware, adminMiddleware);
-router.route('/').post(upload.single('image'), UploadController.uploadProductImage);
+router.route('/').post(Helper.uploadImage().single('image'), UploadController.uploadProductImage);
 
 module.exports = router;
--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -23,17 +23,17 @@ exports.encodePassword = (password) => {
     return bcrypt.hashSync(password, 10);
 }
 
-// exports.uploadFile = () => {
-//     const storage = multer.diskStorage({
-//         destination: function (req, file, cb) {
-//             cb(null, './public/uploads/images/')
-//         },
+exports.uploadImage = () => {
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, './public/uploads/images/')
+        },
 
-//         filename: function (req, file, cb) {
-//             const uniqueSuffix = Date.now() + Math.round(Math.random() * 1E9)
-//             cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-//         }
-//     });
-//     const upload = multer({ storage: storage });
-//     return upload;
-// }
+        filename: function (req, file, cb) {
+            const uniqueSuffix = Date.now() + Math.round(Math.random() * 1E9)
+            cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+        }
+    });
+    const upload = multer({ storage: storage });
+   return upload; 
+}
